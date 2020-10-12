d233e44d44d6bf1b87a811ac7ab8acaedd64cefd
takahiro <takahiro.tsuchiya@sold-out.co.jp>
2020-10-13 00:36:12 +0900

    laravel, vueの基礎作成完了

--- app/routes/web.php
+++ app/routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/{any}', 'SinglePageController@index')->where('any', '.*');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
